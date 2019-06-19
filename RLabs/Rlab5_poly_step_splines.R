4fa6919f9f30d87bf4edc57c16476dce897c08e9
Quaest Pesquisa <quaestpesquisa@Mac-mini-de-Quaest.local>
2019-06-19 12:58:06 -0300

    complementa últimos detalhes

--- RLabs/Rlab5_poly_step_splines.R
+++ RLabs/Rlab5_poly_step_splines.R
@@ -96,38 +96,73 @@ lines(fit,col="red",lwd=2)
 lines(fit2,col="blue",lwd=2)
 legend("topright",legend=c("16 DF","6.8 DF"),col=c("red","blue"),lty=1,lwd=2,cex=.8)
 
-# Local Regression
+## Local Regression
 plot(age,wage,xlim=agelims,cex=.5,col="darkgrey")
 title("Local Regression")
-fit=loess(wage~age,span=.2,data=Wage)
-fit2=loess(wage~age,span=.5,data=Wage)
-lines(age.grid,predict(fit,data.frame(age=age.grid)),col="red",lwd=2)
-lines(age.grid,predict(fit2,data.frame(age=age.grid)),col="blue",lwd=2)
-legend("topright",legend=c("Span=0.2","Span=0.5"),col=c("red","blue"),lty=1,lwd=2,cex=.8)
+
+# span = .2 para definir a vizinhança como 20% dos dados
+fit = loess(wage ~ age, span=.2, data=Wage)
+# span = .5 para definir a vizinhança como 50% dos dados
+fit2 = loess(wage ~ age, span=.5, data=Wage)
+lines(age.grid, predict(fit, data.frame(age=age.grid)), col="red", lwd=2)
+lines(age.grid, predict(fit2, data.frame(age=age.grid)), col="blue", lwd=2)
+legend("topright",
+       legend=c("Span=0.2","Span=0.5"),
+       col=c("red","blue"),
+       lty=1,lwd=2,cex=.8)
 
 
 # GAMs
-
-gam1=lm(wage~ns(year,4)+ns(age,5)+education,data=Wage)
 library(gam)
-gam.m3=gam(wage~s(year,4)+s(age,5)+education,data=Wage)
+
+## Para rodar um GAM usando Natural splines, podemos utilizar
+## o comando nativo de lm
+gam1 = lm(wage ~ ns(year,4) + ns(age,5) + education, data=Wage)
+
+## Para rodar um GAM utilizando smoothing splines
+## (o que não é trivial pois não faz OSL), a função
+## gam() do pacote GAM implementa esse método usando
+## uma abordagem conhecida como backfitting
+## De acordo com os autores do livro ISLR
+## "This method fits a model involving multiple predictors by
+## repeatedly updating the fit for each predictor in turn,
+## holding the others fixed.
+
+gam.m3 = gam(wage ~ s(year,4) + s(age,5) + education, data=Wage)
 par(mfrow=c(1,3))
 plot(gam.m3, se=TRUE,col="blue")
 plot.Gam(gam1, se=TRUE, col="red")
-gam.m1=gam(wage~s(age,5)+education,data=Wage)
-gam.m2=gam(wage~year+s(age,5)+education,data=Wage)
-anova(gam.m1,gam.m2,gam.m3,test="F")
+
+gam.m1 = gam(wage ~ s(age,5) + education, data=Wage)
+gam.m2 = gam(wage ~ year + s(age,5) + education, data=Wage)
+anova(gam.m1,gam.m2,gam.m3,test="F") # Teste de variância
+
 summary(gam.m3)
-preds=predict(gam.m2,newdata=Wage)
-gam.lo=gam(wage~s(year,df=4)+lo(age,span=0.7)+education,data=Wage)
+
+preds = predict(gam.m2, newdata=Wage)
+
+# GAM usando Local Regression como building Block
+gam.lo = gam(wage ~ s(year,df=4) + lo(age,span=0.7) + education,data=Wage)
 plot.Gam(gam.lo, se=TRUE, col="green")
-gam.lo.i=gam(wage~lo(year,age,span=0.5)+education,data=Wage)
+
+gam.lo.i = gam(wage ~ lo(year, age, span=0.5) + education,data=Wage)
+
 library(akima)
+par(mfrow = c(1,2))
 plot(gam.lo.i)
-gam.lr=gam(I(wage>250)~year+s(age,df=5)+education,family=binomial,data=Wage)
+
+## GAM para variáveis binárias
+## Aqui, estamos criando uma variável binária de renda maior
+## que 250 on the fly
+gam.lr = gam(I(wage > 250) ~ year + s(age, df=5) + education,
+             family=binomial, data=Wage)
 par(mfrow=c(1,3))
-plot(gam.lr,se=T,col="green")
+plot(gam.lr, se=T, col="green")
+
 table(education,I(wage>250))
-gam.lr.s=gam(I(wage>250)~year+s(age,df=5)+education,family=binomial,data=Wage,subset=(education!="1. < HS Grad"))
-plot(gam.lr.s,se=T,col="green")
+
+gam.lr.s = gam(I(wage > 250) ~ year + s(age, df=5) + education,
+               family=binomial, data=Wage,
+               subset=(education!="1. < HS Grad"))
+plot(gam.lr.s, se=T, col="green")
 
