0687ae26c16272851a15182fad61c1391512897f
Neylson Crepalde <neylsoncrepalde@Neylsons-MacBook-Air.local>
2019-06-26 17:55:12 -0300

    added bagging and RF

--- RLabs/Rlab6_decision_trees.R
+++ RLabs/Rlab6_decision_trees.R
@@ -87,3 +87,29 @@ abline(0,1)
 mean((yhat - boston.test$medv)^2) #MSE
 
 
+# Bagging and Random Forests
+
+library(randomForest)
+set.seed(1)
+
+bag.boston = randomForest(medv ~., data=Boston, subset=train,
+                          mtry=13, importance=TRUE)
+bag.boston
+yhat.bag = predict(bag.boston, newdata=Boston[-train,])
+
+plot(yhat.bag, boston.test$medv)
+abline(0,1)
+mean((yhat.bag-boston.test$medv)^2)
+
+bag.boston = randomForest(medv~., data=Boston, subset=train,
+                          mtry=13, ntree=25)
+yhat.bag = predict(bag.boston, newdata=Boston[-train,])
+mean((yhat.bag-boston.test$medv)^2)
+
+set.seed(1)
+rf.boston = randomForest(medv~., data=Boston,
+                         subset=train, mtry=6, importance=TRUE)
+yhat.rf = predict(rf.boston,newdata=Boston[-train,])
+mean((yhat.rf-boston.test$medv)^2)
+importance(rf.boston)
+varImpPlot(rf.boston)
