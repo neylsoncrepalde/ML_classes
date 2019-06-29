da34f3b5a73cffdd85d698398fae38a1f68a907b
Neylson Crepalde <neylsoncrepalde@Neylsons-MacBook-Air.local>
2019-06-29 00:48:55 -0300

    last updates

--- RLabs/Rlab6_decision_trees.R
+++ RLabs/Rlab6_decision_trees.R
@@ -8,6 +8,9 @@
 library(tree)
 library(ISLR)
 library(dplyr)
+library(pROC)
+library(readr)
+library(purrr)
 
 ## Fitting Classification Trees ####
 
@@ -25,7 +28,7 @@ Carseats$High = ifelse(Carseats$Sales <= 8, "No", "Yes") %>% as.factor # tem que
 tree.carseats = tree(High ~. -Sales, Carseats)
 summary(tree.carseats)
 plot(tree.carseats)
-text(tree.carseats,pretty=0)
+text(tree.carseats,pretty=1)
 
 # Abordagem do train set test
 set.seed(2)
@@ -39,6 +42,8 @@ tree.pred = predict(tree.carseats, Carseats.test, type="class")
 # Matriz de confusão
 table(tree.pred, Carseats.test$High)
 (104+50)/200  # Acurácia
+
+auc(Carseats.test$High, as.numeric(tree.pred))
 
 # Usando CV para escolher o melhor tamanho de árvore
 set.seed(3)
@@ -58,6 +63,8 @@ tree.pred = predict(prune.carseats, Carseats.test, type="class")
 table(tree.pred,Carseats.test$High)
 (97+58)/200 # Acurácia
 
+auc(Carseats.test$High, as.numeric(tree.pred))
+
 # Tentativa com o outro melhor resultado
 prune.carseats = prune.misclass(tree.carseats, best=15)
 plot(prune.carseats)
@@ -65,6 +72,8 @@ text(prune.carseats, pretty=0)
 tree.pred = predict(prune.carseats, Carseats.test, type="class")
 table(tree.pred,Carseats.test$High)
 (102+53)/200 # Acurácia
+
+auc(Carseats.test$High, as.numeric(tree.pred))
 
 ## Fitting Regression Trees ####
 # Vamos trabalhar com o banco de dados Boston
@@ -153,3 +162,80 @@ boost.boston = gbm(medv~.,
 # Verifica o erro de teste
 yhat.boost = predict(boost.boston, newdata=Boston[-train,], n.trees=5000)
 mean((yhat.boost-boston.test$medv)^2)
+
+
+#### Aplicações
+# Random Forest Classifier: Credit Data
+credit = read_csv("https://github.com/neylsoncrepalde/RFSVM/blob/master/UCI_Credit_Card.csv?raw=true")
+credit$SEX = factor(credit$SEX, levels = c(1, 2), labels = c("Male", "Female"))
+credit$EDUCATION = factor(credit$EDUCATION, levels = c(1,2,3,4,5,6),
+                          labels = c("Grad School", "University", "High School",
+                                     "Others", "Unknown", "Unknown"))
+credit$MARRIAGE = factor(credit$MARRIAGE, levels = c(1,2,3),
+                         labels = c("Married", "Single", "Other"))
+credit$default.payment.next.month = factor(credit$default.payment.next.month,
+                                           levels = c(0,1), labels = c("No", "Yes"))
+
+table(credit$PAY_0)
+
+## Verificando a variável resposta
+table(credit$default.payment.next.month)
+prop.table(table(credit$default.payment.next.month))*100
+
+# Missing treatment
+dim(credit)
+dim(na.omit(credit))
+# Não há tanto problema em jogar os missings fora
+
+credit = na.omit(credit)
+
+## Divide train test
+set.seed(5)
+train = sample(1:nrow(credit), nrow(credit)*.7)
+
+fit = randomForest(default.payment.next.month ~. - ID, 
+                   data=credit, 
+                   subset=train,
+                   mtry=5,   # sqrt(p) 
+                   importance=TRUE)
+fit
+
+importance(fit)
+varImpPlot(fit)
+
+## Calculate train error and test error
+yhattrain = predict(fit, type = "class")
+yhattest = predict(fit, newdata = credit[-train, ], type = "class")
+
+# Matriz de confusão
+table(credit$default.payment.next.month[train], yhattrain)
+table(credit$default.payment.next.month[-train], yhattest)
+
+# Erro de treino
+auc(credit$default.payment.next.month[train], as.numeric(yhattrain))
+# Erro de teste
+auc(credit$default.payment.next.month[-train], as.numeric(yhattest))
+
+
+### Random Forests Regression
+## Prevendo o limite de crédito
+fit2 = randomForest(LIMIT_BAL ~. -ID - default.payment.next.month, 
+                    data=credit, subset = train,
+                    mtry=5, importance=T, ntree=1000)
+fit2
+importance(fit2)
+varImpPlot(fit2)
+
+yhattrain = predict(fit2)
+yhattest = predict(fit2, newdata = credit[-train, ])
+
+## RMSE
+sqrt(mean((credit$LIMIT_BAL[train] - yhattrain)^2))
+
+sqrt(mean((credit$LIMIT_BAL[-train] - yhattest)^2))
+
+plot(fit2)
+
+
+
+
